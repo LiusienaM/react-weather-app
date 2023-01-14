ceae1fc2f14246cc5aae6ab88a8e53624a48c120
Liusiena Martyniuk <lusenamartynuk@gmail.com>
2023-01-14 20:03:54 +0200

    Added 7 days of forecast

--- src/FormatedDate.js
+++ src/FormatedDate.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function FormatedDate(props) {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
   if (hours < 10) {
