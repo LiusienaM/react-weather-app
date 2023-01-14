5bb5fef9e7a048bb1eeaf56e38886e2eb0a3ed25
Liusiena Martyniuk <lusenamartynuk@gmail.com>
2023-01-14 20:15:28 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,30 @@ function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="London" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://luminous-gingersnap-d193d9.netlify.app/"
+            target="_blank"
+          >
+            Liusiena M
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/LiusienaM/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://ornate-paprenjak-36607c.netlify.app/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
